314db4a0d74d287b0451a27709983ea7a07aface
zarmir <pillihumanhz@gmail.com>
2019-05-09 17:31:07 -0500

    descr

--- src/main/java/common/system/Controller/ImagenEntityController.java
+++ src/main/java/common/system/Controller/ImagenEntityController.java
@@ -2,6 +2,9 @@ package common.system.Controller;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.core.io.ClassPathResource;
@@ -9,10 +12,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import domain.System.BusinessEntity.ViewStockBE;
 import domain.System.BusinessEntity.Base.Clothingline;
@@ -20,19 +29,21 @@ import domain.System.BusinessEntity.Base.Imagen;
 import repository.System.DataAccess.MySql.ImagenDa;
 @RestController
 public class ImagenEntityController {
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "C://Users//pilli//Pictures//";
 	   @RequestMapping(value = "/sid", method = RequestMethod.GET,
 	            produces = MediaType.IMAGE_JPEG_VALUE)
 	    public ResponseEntity<byte[]> getImage() throws IOException {
 
 	    	ClassPathResource imgFile = new ClassPathResource("image/ropa.jpeg");
-	        //byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+	        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
 	        ImagenDa obj = new ImagenDa();
  	        List<Imagen> imf  = null;
-	        byte[] bytes= null;
+	        //byte[] bytes= null;
 	        imf= obj.list();
-	        for (Imagen imagen : imf) {
-	         bytes=imagen.getImagendata(); 
-			}
+	        //for (Imagen imagen : imf) {
+	         //bytes=imagen.getImagendata(); 
+			//}
 	        return ResponseEntity
 	                .ok()
 	                .contentType(MediaType.IMAGE_JPEG)
@@ -45,5 +56,36 @@ public class ImagenEntityController {
 	    	return new ModelAndView("RegisterImagen", "command", new ViewStockBE());
 	    }
 	  
-	    	   
+	    @PostMapping("/upload") // //new annotation since 4.3
+	    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+	                                   RedirectAttributes redirectAttributes) {
+
+	        if (file.isEmpty()) {
+	            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+	            return "redirect:uploadStatus";
+	        }
+
+	        try {
+
+	            // Get the file and save it somewhere
+	            byte[] bytes = file.getBytes();
+	            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+	            Files.write(path, bytes);
+
+	            redirectAttributes.addFlashAttribute("message",
+	                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        return "redirect:/uploadStatus";
+	    }
+	    
+	    @GetMapping("/uploadStatus")
+	    public String uploadStatus() {
+	        return "uploadStatus";
+	    }
+
+	   
 }
